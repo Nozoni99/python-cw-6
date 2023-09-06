614e14a4272e7099bcc25d707dde27b134606ab9
Nozoni99 <noorwaleed206@gmail.com>
2023-09-06 19:40:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 # write your code here
+person={
+    "name":"Noor",
+    "age":17 ,
+    "hobbies": ["Reading","Cooking","Watching TV","Writing"]
+
+       }
+
+print(person["name"])
+print(person["age"])
+
+################################################################
+
+person["age"]=18
+person["country"]="Egypt"
+
+print(person)
+
+print(len(person))
+
+################################################################
+
+person["hobbies"].append("Singing")
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW your are amazing !!!")
+    else:
+        print("You better start new hobbies .")
+
+check_hobbies(person) 
+    
+
+
+
+
+
